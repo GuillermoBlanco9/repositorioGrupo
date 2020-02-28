8533a92a5581112c8969663206d182a482e11f7b
luismp29 <60668091+luismp29@users.noreply.github.com>
2020-02-28 19:40:49 +0100

    bug

--- Factura.java
+++ Factura.java
@@ -5,7 +5,7 @@ public class Factura implemets iFactura
 	public int num;
 	public float base;
 	public float tipoIva;
-	//public Estados estado;
+	protected Estados estado;
 	
 	public static void main(String[] args) 
 	{
